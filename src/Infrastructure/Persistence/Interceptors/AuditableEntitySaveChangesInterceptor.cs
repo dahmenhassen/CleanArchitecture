ae2acdd7c02a6de85e06c8256b1271c3012e8983
Moein Aghamirzaei <moein.6556@gmail.com>
2023-03-16 06:29:49 +0330

    auto clean

--- src/Infrastructure/Persistence/Interceptors/AuditableEntitySaveChangesInterceptor.cs
+++ src/Infrastructure/Persistence/Interceptors/AuditableEntitySaveChangesInterceptor.cs
@@ -58,14 +58,3 @@ public class AuditableEntitySaveChangesInterceptor : SaveChangesInterceptor
         }
     }
 }
-
-public static class Extensions
-{
-    public static bool HasChangedOwnedEntities(this EntityEntry entry)
-    {
-        return entry.References.Any(r =>
-            r.TargetEntry != null &&
-            r.TargetEntry.Metadata.IsOwned() &&
-            (r.TargetEntry.State == EntityState.Added || r.TargetEntry.State == EntityState.Modified));
-    }
-}
